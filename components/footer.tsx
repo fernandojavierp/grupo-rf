9561afa5c88276d371926ab71faa4b52a6adbbf7
Fernando <fpatetegonzalez@gmail.com>
2025-07-07 15:14:59 +0200

    se ha actualizado los datos de contacto y descripcion y arreglado algunos bugs de animacion-1

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import { Facebook, Instagram, Mail, Phone, MapPin } from "lucide-react"
+import { Instagram, Mail, Phone, MapPin } from "lucide-react"
 
 export default function Footer() {
   return (
